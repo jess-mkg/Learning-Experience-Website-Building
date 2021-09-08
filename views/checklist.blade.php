a0d450c89aecfb78e1fec77329c2d84b4035d5a4
jess-mkg <78837964+jess-mkg@users.noreply.github.com>
2021-09-08 21:16:23 +1000

    Add files via upload

--- views/checklist.blade.php
+++ views/checklist.blade.php
@@ -14,9 +14,22 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    {{ __('Checklist Completed: ') }}
+                    {{ __('') }}
                 </div>
             </div>
+            @if(Auth::user()->role == 'Parent')
+                <form style="text-align: center; margin: 15px; font-size: 130%">
+                    <a href="/checklist-parents"><input type="button" value="Begin"/></a>
+                </form>
+            @elseif(Auth::user()->role == 'Teacher')
+            <form style="text-align: center; margin: 15px; font-size: 130%">
+                    <a href="/checklist-teachers"><input type="button" value="Begin"/></a>
+            </form>
+            @else
+            <form style="text-align: center; margin: 15px; font-size: 130%">
+                    <a href="/checklist"><input type="button" value="Begin"/></a>
+            </form>
+            @endif
         </div>
     </div>
 </div>
